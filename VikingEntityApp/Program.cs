70aba80d3cb53458ec862b9da6cf508e1f1fa2f0
Arthur Frey <afrey@steelcloud.com>
2023-01-31 12:34:28 -0500

    comeplete

--- VikingEntityApp/Program.cs
+++ VikingEntityApp/Program.cs
@@ -7,11 +7,17 @@ static class Program
 {
     public static Settings _settings;
     public static UserBase _userBase;
+    public static User _currentUser = new User();
     static void Main(string[] p_args)
     {
         AppFiles.Create();
         _settings = new Settings();
         _userBase = new UserBase();
+
+        while(_currentUser.Oid == 0)
+        {
+            DoLogin();
+        }
 
 
         var mainMenu = new Views.Main();
@@ -22,5 +28,26 @@ static class Program
         
         Console.WriteLine("Press any key to exit.");
         Console.ReadKey();
+    }
+    
+    private static void DoLogin()
+    {
+        string user = "";
+        string pass = "";
+        while (user == "" || pass == "")
+        {
+            Console.Write("Username: ");
+            user = Console.ReadLine();
+            Console.Write("Password: ");
+            pass = Console.ReadLine();
+        }
+        
+        _currentUser = _userBase.Login(user, pass, out string message);
+
+        if (message != "")
+        {
+            Messages.Error($"Login Error: {message}");
+        }
+        
     }
 }
